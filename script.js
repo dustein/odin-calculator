72df622f48d1a522608e6174fe4317c087ce5a45
DuStein <steindu@gmail.com>
2022-08-11 11:01:34 -0300

    back note home

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function soma(num1, num2) {
+  return num1 + num2;
+}
+
+function subtrai(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplica(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function tecla(e) {
+  const teste = document.querySelector('data-key="1"')
+  consolelog(teste)
+}
