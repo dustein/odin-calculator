7cdf286432ee9070355ca28759889bfe9f1beb92
DuStein <steindu@gmail.com>
2022-08-14 17:49:06 -0300

    no sucess yet

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 
-function soma(num1, num2) {
-  return num1 + num2;
-}
+// function soma(num1, num2) {
+//   return num1 + num2;
+// }
 
-function subtrai(num1, num2) {
-  return num1 - num2;
-}
+// function subtrai(num1, num2) {
+//   return num1 - num2;
+// }
 
-function multiplica(num1, num2) {
-  return num1 * num2;
-}
+// function multiplica(num1, num2) {
+//   return num1 * num2;
+// }
 
-function divide(num1, num2) {
-  return num1 / num2;
-}
+// function divide(num1, num2) {
+//   return num1 / num2;
+// }
 
 
-const teclas = Array.from(document.querySelectorAll('.teclas'));
-console.log(teclas)
-
-teclas.forEach(tecla => {
-  tecla.addEventListener('click', (e) => {
-    console.log("teste")
-  })
-})
+const botoes = document.querySelectorAll('.tecla')
+console.log(botoes)
