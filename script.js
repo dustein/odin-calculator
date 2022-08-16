f6226e5196fee875076c3800018da7482c5356d5
DuStein <steindu@gmail.com>
2022-08-15 22:03:20 -0300

    iniciando javascript

--- script.js
+++ script.js
@@ -14,5 +14,11 @@
 // function divide(num1, num2) {
 //   return num1 / num2;
 // }
-
-
+const display = document.querySelector('#numero');
+const pressKey = document.querySelectorAll('.tecla');
+pressKey.forEach(tecla => {
+  tecla.addEventListener('click', () => {
+    console.log(tecla.innerHTML)
+    display.innerText += tecla.innerText;
+  })
+})
